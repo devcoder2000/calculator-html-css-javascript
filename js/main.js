560002e8e3be679bd775717a942e1ba7e9fb7ab2
devcoder2000 <web.coder.2000@gmail.com>
2023-02-24 20:12:52 -0600

    javascript

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ botones.forEach(boton => {
         }
 
         if(boton.id === "borrar"){
-            if(pantalla.textContent.length === 1){
+            if(pantalla.textContent.length === 1 || pantalla.textContent === "Error!"){
                 pantalla.textContent = "0";
             }else{
                 pantalla.textContent = pantalla.textContent.slice(0, -1);
@@ -19,7 +19,16 @@ botones.forEach(boton => {
             return;
         }
 
-        if(pantalla.textContent === "0") {
+        if(boton.id === "igual"){
+            try{
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch{
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+
+        if(pantalla.textContent === "0" || pantalla.textContent === "Error!") {
             pantalla.textContent = botonApretado;
         }else {
             pantalla.textContent += botonApretado;
